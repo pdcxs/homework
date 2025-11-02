4ea21fee45a0734aa0284d9057df123ef5580cc3
pdcxs <pdcxs@outlook.com>
2025-11-02 13:10:36 +0800

    change base path

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/homework/',
   test: {
     globals: true,
     environment: 'jsdom',
